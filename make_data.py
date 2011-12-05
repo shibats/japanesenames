e3aa0c6bc2cda0cde848f762f2652fc1ec2ff610
Atsushi Shibata <shibata@webcore.co.jp>
2011-12-05 11:55:13 +0900

    changed to skip names containing invalid kanji characters, sort resutl before writing.

--- make_data.py
+++ make_data.py
@@ -12,6 +12,17 @@ from scrapers import scraper1, scraper2, scraper3
 #
 # The scrape() function should wait while every time after reading a resource.
 
+# reading kakusuu data to make dictionary.
+
+import codecs
+
+kd = {}
+f = codecs.open('./data/kakusu.tsv', 'r', 'utf-8')
+for line in f:
+    l = line.split()
+    if len(l) >= 2:
+        kd[l[1]] = 1
+
 
 # making the unique list of boys name
 
@@ -21,8 +32,16 @@ s3 = set(scraper3.scrape(0))
 
 s1.update(s2)
 s1.update(s3)
+
+# sort, validate
+bl = [x for x in list(s1) if len(x.split()) >= 2]
+bl.sort(lambda x, y: cmp(x.split()[1], y.split()[1]))
+for i in bl:
+    if not [x for x in i.split()[0] if x in kd]:
+        bl.remove(i)
+
 f = open('./data/bname.tsv', 'w')
-f.write('\n'.join(s1))
+f.write('\n'.join(bl))
 
 
 # making the unique list of girls name
@@ -33,5 +52,13 @@ s3 = set(scraper3.scrape(1))
 
 s1.update(s2)
 s1.update(s3)
+
+# sort, validate
+gl = [x for x in list(s1) if len(x.split()) >= 2]
+gl.sort(lambda x, y: cmp(x.split()[1], y.split()[1]))
+for i in gl:
+    if not [x for x in i.split()[0] if x in kd]:
+        gl.remove(i)
+
 f = open('./data/gname.tsv', 'w')
-f.write('\n'.join(s1))
+f.write('\n'.join(gl))
